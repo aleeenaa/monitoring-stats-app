d26dd1e4d26909bcfcef2c5edffeed0ba0cfdf15
A Naeem <aleena.naeem@capgemini.com>
2020-10-09 19:42:18 +0100

    Added express

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+import express from 'express'
 import { generateData } from './scripts/helper.js'
 
-main = () => {
-    data = generateData(5)
-}
+let app = express()
+
+let data = generateData(5)
+
+app.set("port", (process.env.PORT || 8080))
+app.use(bodyParser.json({
+    type: "application/json"
+}))
+
+app.get('/', (request, result) => {
+    result.send('Hello World!\n'+JSON.parse(data))
+})
+
+// Start the server
+app.listen(app.get('port'), () => {
+    require('dns').lookup(require('os').hostname(),
+        (err, addr) => {
+        console.log(
+            `App listening at http://${addr}:${process.env.PORT || 8080}\n` +
+            'Press Ctrl+C to quit.'
+        )
+    })
+})
