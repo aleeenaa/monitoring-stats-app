c0d2f32d47d86b33934d378fc2651e2f6810b599
barthola <up716038@myport.ac.uk>
2020-10-12 16:59:21 +0100

    Added helper functions to query data

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 import express from 'express'
-import generateData from './scripts/helper.js'
+import { generateData, getSeconds, validateInput, queryData } from './scripts/helper.js'
 
-let app = express()
+const app = express(), data = generateData(5), PORT = (process.env.PORT || 8080)
 
-let data = generateData(5)
+app.set("port", PORT)
+app.use(express.json())
 
-app.set("port", (process.env.PORT || 8080))
-app.use(express.json)
-
-app.get('/', (request, result) => {
-    result.send('Hello World!\n'+JSON.parse(data))
+app.get('/search', (req, res) => {
+    const params = req.params
+    let result
+    if (!validateInput(params)) {
+        result = {
+            status: 'error',
+            statusMessage: 'Missing or incorrect user input'
+        }
+    } else {
+        result = queryData(getSeconds(params.start), getSeconds(params.end), data)
+    }
+    res.send(result)
 })
 
 // Start the server
 app.listen(app.get('port'), () => {
-    console.log(`App listening at http://localhost:${process.env.PORT}\n` +
-        'Press Ctrl+C to quit.')
-    console.log('data', JSON.stringify(data))
+    console.log(`App listening at http://localhost:${PORT}\n` +
+        'Press Ctrl+C to quit.\n')
 })
--- scripts/helper.js
+++ scripts/helper.js
@@ -1,20 +1,38 @@
-const generateData = (mins) => {
-    let currentTime = Math.floor(Date.now() * 0.0001) *10;
-    let data = [];
+export const generateData = (mins) => {
+    let currentTime = getSeconds(Date.now())
+    let data = []
 
     for (let i = 0; i < mins; i++) {
         data.push({
             timestamp: currentTime - (i*60),
             cpuLoad: getRandomIntFromRange(0, 100),
             concurrency: getRandomIntFromRange(0, 500000)
-        });
+        })
     }
 
-    return data;
+    return data
+}
+
+export const getSeconds = (dt) => {
+    return  Math.floor(new Date(dt) * 0.0001) *10
 }
 
 const getRandomIntFromRange = (start, end) => {
-    return ~~(Math.random() * end) + start;
+    return ~~(Math.random() * end) + start
 }
 
-export default generateData
+export const validateInput = (params) => {
+    if (!params || !params.start || !params.end) {
+        return false
+    }
+    const start = new Date(params.start) != "undefined", end = new Date(params.end) != "undefined"
+    return (start && end)
+}
+
+export const queryData = (start, end, data) => {
+    data.sort()
+    data.filter(obj => {
+        return start <= obj.timestamp <= end
+    })
+    return data
+}
